091f18fa2ab490d8365c2bcaed44a49706d0fd10
Brennan Seymour <bseymour@sourceallies.com>
2023-07-19 14:57:55 -0500

    block absolute paths

--- badgenerator.py
+++ badgenerator.py
@@ -5,25 +5,37 @@ path = os.getenv("PATH")
 patharr = path.split(":")
 
 bins = []
+filter_relative_list = [
+    "alias",
+    "echo",
+]
+
+filter_absolute_list = [
+    "starship"
+]
 
 for pathdir in patharr:
     try:
         files = os.listdir(pathdir)
-        bins.extend(files)
+        for file in files:
+            if file not in filter_relative_list:
+                bins.append(file)
+            if file not in filter_absolute_list:
+                bins.append(f"{pathdir}/{file}")
     except:
         print(f"messed up on: {pathdir}")
-
-bins = list(filter(lambda x: x != "alias" and x != "echo", bins))
 
 # add some builtins
 bins.append("exit")
 bins.append("source")
 
+# prevent using absolute paths
+bins.append("/")
 # re-add echo and alias at the end
 #bins.append("echo")
 bins.append("alias")
 
-pattern = re.compile("^[a-zA-Z-]*$")
+pattern = re.compile("^[a-zA-Z-\/]*$")
 with open("badrc", "w") as f:
     for bin in bins:
         if pattern.match(bin):
